578ca2aa9879adc347c48c3ec0a3509542607831
Marcel Widmer <marzelwidmer@gmail.com>
2019-04-06 00:26:26 +0200

    Polishing.

--- README.md
+++ README.md
@@ -1,2 +1,69 @@
 # feature-toggle-demo
 feature toggle demo app
+
+
+## Start Config Server
+```bash
+$ spring cloud configserver
+```
+
+### Test Config Server
+```bash
+$ http :8888/feature-toggle-demo/deafult
+```
+
+```bash
+HTTP/1.1 200
+Content-Type: application/json;charset=UTF-8
+Date: Fri, 05 Apr 2019 21:59:48 GMT
+Transfer-Encoding: chunked
+
+{
+    "label": null,
+    "name": "feature-toggle-demo",
+    "profiles": [
+        "deafult"
+    ],
+    "propertySources": [
+        {
+            "name": "file:///Users/marcelwidmer/dev/git/feature-toggle-demo/config-repo/feature-toggle-demo.yml",
+            "source": {
+                "app.feature": "feature1",
+                "logging.pattern.console": "%clr(%d{yy-MM-dd E HH:mm:ss.SSS}){blue} %clr(%-5p) %clr(${PID}){faint} %clr([${spring.zipkin.service.name:${spring.application.name:-}},){magenta}%clr(%X{X-B3-TraceId:-},){yellow}%clr(%X{X-B3-SpanId:-},){cyan}%clr(,%X{X-Span-Export:-}]){blue}%clr([%8.15t]){cyan} %clr(%-40.40logger{0}){blue} %clr(:){red} %clr(%m){faint}%n",
+                "server.port": 8081
+            }
+        }
+    ],
+    "state": null,
+    "version": "7f7424704be2f49b91ac56a251a2ab696cc7d85a"
+}
+```
+
+## Test Feature Toggle
+```bash
+$ http :8081/api
+```
+Check if there the _chuck-norris_ link.
+
+```bash
+HTTP/1.1 200
+Content-Type: application/hal+json;charset=UTF-8
+Date: Fri, 05 Apr 2019 21:53:29 GMT
+Transfer-Encoding: chunked
+
+{
+    "_links": {
+        "chuck-norris": {
+            "href": "http://api.icndb.com/jokes/random"
+        },
+        "customer": {
+            "href": "http://localhost:8081/api/customer"
+        },
+        "greeting": {
+            "href": "http://localhost:8081/api/greeting"
+        },
+        "self": {
+            "href": "http://localhost:8081/api"
+        }
+    }
+```
--- src/main/kotlin/ch/keepcalm/demo/featuretoggle/Application.kt
+++ src/main/kotlin/ch/keepcalm/demo/featuretoggle/Application.kt
@@ -5,17 +5,17 @@ import org.springframework.boot.runApplication
 import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
- import org.springframework.beans.factory.annotation.Value
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Primary
 import org.springframework.hateoas.*
 import org.springframework.hateoas.config.EnableHypermediaSupport
 import org.springframework.hateoas.server.mvc.add
-import org.springframework.stereotype.Service
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.boot.autoconfigure.condition.ConditionalOnExpression
- import org.springframework.stereotype.Component
+import org.springframework.cloud.context.config.annotation.RefreshScope
+import org.springframework.stereotype.Component
 
 
 fun main(args: Array<String>) {
@@ -24,61 +24,7 @@ fun main(args: Array<String>) {
 
 @SpringBootApplication
 @EnableHypermediaSupport(type = arrayOf(EnableHypermediaSupport.HypermediaType.HAL))
-class Application() {
-    @Bean
-    @Primary
-    @ConditionalOnProperty(prefix = "feature.toggle", name = ["holidaySeason"], havingValue = "false")
-    fun getStandardGreetingHandler(): GreetingHandler {
-        return StandardGreetingHandler()
-    }
-
-    @Bean
-    @ConditionalOnProperty(prefix = "feature.toggle", name = ["holidaySeason"], havingValue = "true")
-    fun getGreetingHandler(): GreetingHandler {
-        return HolidayGreetingHandler()
-    }
-}
-
-@Component
-@ConditionalOnExpression("\${feature.toggle:false}")
-data class Feature1(val name: String = "feature1")
-
-
-
-interface GreetingHandler {
-    fun getGreeting(name: String): String
-}
-
-class StandardGreetingHandler : GreetingHandler {
-    override fun getGreeting(name: String): String {
-        return String.format(template, name)
-    }
-
-    companion object {
-        private val template = "Hello, %s!"
-    }
-}
-
-@RestController
-@RequestMapping("/api", produces = [MediaTypes.HAL_JSON_UTF8_VALUE])
-class GreetingController (private val handler: GreetingHandler) {
-
-    @RequestMapping("/greeting")
-    fun greeting(@RequestParam(value = "name", defaultValue = "World") name: String): String {
-        return handler.getGreeting(name)
-    }
-}
-
-class HolidayGreetingHandler : GreetingHandler {
-
-    override fun getGreeting(name: String): String {
-        return String.format(template, name)
-    }
-
-    companion object {
-        private val template = "Happy Holidays, %s!"
-    }
-}
+class Application() {}
 
 
 //   _   _    _  _____ _____ ___    _    ____
@@ -91,6 +37,7 @@ open class Index : RepresentationModel<Index>()
 
 @RestController
 @RequestMapping("/api", produces = [MediaTypes.HAL_JSON_UTF8_VALUE])
+@RefreshScope
 class IndexController {
 
     @Value("\${app.feature}")
@@ -101,38 +48,11 @@ class IndexController {
             .apply {
                 add(IndexController::class) {
                     linkTo { api() } withRel IanaLinkRelations.SELF
-                    linkTo { api() }.slash("greeting") withRel "greeting"
-                }
-                add(CustomerController::class) {
-                    linkTo { getCustomer() } withRel "customer"
-                }
-                when (feature){
-                    "feature1" -> add(Link("http://api.icndb.com/jokes/random" , "chuck-norris"))
-                    "feature2" -> add(Link("http:/google.com" , "google"))
-                    "feature3" -> add(Link("https://api.opendota.com/api/heroStats" , "heros"))
+                    when (feature) {
+                        "feature1" -> add(Link("http://api.icndb.com/jokes/random", "chuck-norris"))
+                        "feature2" -> add(Link("http:/google.com", "google"))
+                        "feature3" -> add(Link("https://api.opendota.com/api/heroStats", "heros"))
+                    }
                 }
             }
-}
-
-@Service
-class CustomerService {
-    fun getCustomer(): CustomerModel = CustomerModel(firstname = "John", lastname = "Doe")
-}
-
-open class CustomerModel(
-        val firstname: String,
-        val lastname: String
-) : RepresentationModel<CustomerModel>()
-
-
-@RequestMapping("/api")
-@RestController
-class CustomerController(private val customerService: CustomerService) {
-
-    @GetMapping(value = ["/customer"], produces = [MediaTypes.HAL_JSON_UTF8_VALUE])
-    fun getCustomer(): CustomerModel {
-        return customerService.getCustomer()
-    }
-
-
 }
